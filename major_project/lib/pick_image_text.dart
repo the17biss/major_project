53e3b5518fd1453e039515b6d2dfbdb61af107e5
Bishal <b.humagai17@gmail.com>
2022-12-31 19:34:19 +0545

    Text Recognition Using Pytesseract

--- major_project/lib/pick_image_text.dart
+++ major_project/lib/pick_image_text.dart
@@ -1,14 +1,11 @@
-import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
 import 'dart:io';
+import 'package:flutter/material.dart';
+import 'package:google_ml_kit/google_ml_kit.dart';
 import 'package:image_picker/image_picker.dart';
 import 'package:flutter_tts/flutter_tts.dart';
-import 'package:path_provider/path_provider.dart';
-// ignore: depend_on_referenced_packages
-import 'package:path/path.dart';
 
 class PickImageText extends StatefulWidget {
-  const PickImageText({super.key});
+  const PickImageText({Key? key}) : super(key: key);
 
   @override
   State<PickImageText> createState() => _PickImageTextState();
@@ -17,139 +14,168 @@ class PickImageText extends StatefulWidget {
 class _PickImageTextState extends State<PickImageText> {
   final FlutterTts flutterTts = FlutterTts();
 
-  File? _image;
+  bool textScanning = false;
 
-  Future _openCamera() async {
-    try {
-      final image = await ImagePicker().pickImage(source: ImageSource.camera);
-      if (image == null) return;
+  XFile? imageFile;
 
-      final imagePermanent = await saveFilePermanently(image.path);
-      //final imageTemporary = File(image.path);
-      setState(() {
-        _image = imagePermanent;
-      });
-    } on PlatformException catch (e) {
-      print("Failed to pick image: $e");
-    }
-  }
-
-  Future _openGallery() async {
-    try {
-      final image = await ImagePicker().pickImage(source: ImageSource.gallery);
-      if (image == null) return;
-
-      final imagePermanent = await saveFilePermanently(image.path);
-      //final imageTemporary = File(image.path);
-      setState(() {
-        _image = imagePermanent;
-      });
-    } on PlatformException catch (e) {
-      print("Failed to pick image: $e");
-    }
-  }
-
-  Future<File> saveFilePermanently(String imagePath) async {
-    final directory = await getApplicationDocumentsDirectory();
-    final name = basename(imagePath);
-    final image = File('${directory.path}/$name');
-
-    return File(imagePath).copy(image.path);
-  }
+  String scannedText = "";
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text('Assistive App'),
-          backgroundColor: Colors.blueAccent,
-        ),
-        body: Column(children: [
-          Container(
-            width: double.infinity,
-            color: const Color.fromARGB(255, 255, 255, 255),
-            margin: const EdgeInsets.all(10),
-            child: const Text(
-              'Pick Image',
-              style: TextStyle(
-                fontSize: 25,
-                fontWeight: FontWeight.bold,
-                color: Color.fromARGB(255, 17, 0, 0),
-              ),
-              textAlign: TextAlign.center,
-            ),
-          ),
-          Container(
-            width: double.infinity,
-            height: 75,
-            margin: const EdgeInsets.all(10),
-            child: GestureDetector(
-                child: ElevatedButton(
-                  style: ButtonStyle(
-                      backgroundColor: MaterialStateProperty.all<Color>(
-                          const Color.fromARGB(255, 92, 224, 198))),
-                  child: const Text(
-                    'Pick From Gallery',
-                    style: TextStyle(
-                      fontSize: 25,
-                      color: Color.fromARGB(255, 17, 0, 0),
-                    ),
-                    textAlign: TextAlign.center,
-                  ),
-                  onPressed: () async {
-                    await flutterTts.setLanguage("en-US");
-                    await flutterTts.speak("Pick From Gallery");
-                  },
-                ),
-                onDoubleTap: () {
-                  _openGallery();
-                }),
-          ),
-          Container(
-            width: double.infinity,
-            height: 75,
-            margin: const EdgeInsets.all(10),
-            child: GestureDetector(
-                child: ElevatedButton(
-                  style: ButtonStyle(
-                      backgroundColor: MaterialStateProperty.all<Color>(
-                          const Color.fromARGB(255, 243, 72, 72))),
-                  child: const Text(
-                    'Pick From Camera',
-                    style: TextStyle(
-                      fontSize: 25,
-                      color: Color.fromARGB(255, 17, 0, 0),
-                    ),
-                    textAlign: TextAlign.center,
-                  ),
-                  onPressed: () async {
-                    await flutterTts.setLanguage("en-US");
-                    await flutterTts.speak("Pick From Camera");
-                  },
-                ),
-                onDoubleTap: () {
-                  _openCamera();
-                }),
-          ),
-          Container(
-              width: double.infinity,
-              height: 300,
-              margin: const EdgeInsets.all(10),
-              child: _image != null
-                  ? Image.file(
-                      _image!,
-                      width: 1000,
-                      height: 1000,
-                      fit: BoxFit.fitWidth,
-                    )
-                  : Image.network(
-                      'https://media.sproutsocial.com/uploads/2017/02/10x-featured-social-media-image-size.png',
-                      width: 1000,
-                      height: 1000,
-                      fit: BoxFit.fitWidth)),
-        ]),
+    return Scaffold(
+      appBar: AppBar(
+        centerTitle: true,
+        title: const Text("Text Recognition example"),
       ),
+      body: Center(
+          child: SingleChildScrollView(
+        child: Container(
+            margin: const EdgeInsets.all(20),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                if (textScanning) const CircularProgressIndicator(),
+                if (!textScanning && imageFile == null)
+                  Container(
+                    width: 300,
+                    height: 300,
+                    color: Colors.grey[300]!,
+                  ),
+                if (imageFile != null) Image.file(File(imageFile!.path)),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Container(
+                        margin: const EdgeInsets.symmetric(horizontal: 5),
+                        padding: const EdgeInsets.only(top: 10),
+                        child: ElevatedButton(
+                          style: ElevatedButton.styleFrom(
+                            foregroundColor: Colors.grey,
+                            backgroundColor: Colors.white,
+                            shadowColor: Colors.grey[400],
+                            elevation: 10,
+                            shape: RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(8.0)),
+                          ),
+                          onPressed: () async {
+                            await flutterTts.setLanguage("en-US");
+                            await flutterTts.speak(
+                                "Long Press to select picture from gallery");
+                          },
+                          onLongPress: () {
+                            getImage(ImageSource.gallery);
+                          },
+                          child: Container(
+                            margin: const EdgeInsets.symmetric(
+                                vertical: 5, horizontal: 5),
+                            child: Column(
+                              mainAxisSize: MainAxisSize.min,
+                              children: [
+                                const Icon(
+                                  Icons.image,
+                                  size: 30,
+                                ),
+                                Text(
+                                  "Gallery",
+                                  style: TextStyle(
+                                      fontSize: 13, color: Colors.grey[600]),
+                                )
+                              ],
+                            ),
+                          ),
+                        )),
+                    Container(
+                        margin: const EdgeInsets.symmetric(horizontal: 5),
+                        padding: const EdgeInsets.only(top: 10),
+                        child: ElevatedButton(
+                          style: ElevatedButton.styleFrom(
+                            foregroundColor: Colors.grey,
+                            backgroundColor:
+                                const Color.fromARGB(255, 242, 244, 246),
+                            shadowColor: Colors.grey[400],
+                            elevation: 10,
+                            shape: RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(8.0)),
+                          ),
+                          onPressed: () async {
+                            await flutterTts.setLanguage("en-US");
+                            await flutterTts.speak(
+                                "Long Press to click picture using camera");
+                          },
+                          onLongPress: () {
+                            getImage(ImageSource.camera);
+                          },
+                          child: Container(
+                            margin: const EdgeInsets.symmetric(
+                                vertical: 5, horizontal: 5),
+                            child: Column(
+                              mainAxisSize: MainAxisSize.min,
+                              children: [
+                                const Icon(
+                                  Icons.camera_alt,
+                                  size: 30,
+                                ),
+                                Text(
+                                  "Camera",
+                                  style: TextStyle(
+                                      fontSize: 13, color: Colors.grey[600]),
+                                )
+                              ],
+                            ),
+                          ),
+                        )),
+                  ],
+                ),
+                const SizedBox(
+                  height: 20,
+                ),
+                Text(
+                  scannedText,
+                  style: const TextStyle(fontSize: 20),
+                )
+              ],
+            )),
+      )),
     );
+  }
+
+  void getImage(ImageSource source) async {
+    try {
+      final pickedImage = await ImagePicker().pickImage(source: source);
+      if (pickedImage != null) {
+        textScanning = true;
+        imageFile = pickedImage;
+        setState(() {});
+        getRecognisedText(pickedImage);
+      }
+    } catch (e) {
+      textScanning = false;
+      imageFile = null;
+      scannedText = "Error occured while scanning";
+      setState(() {});
+    }
+  }
+
+  void getRecognisedText(XFile image) async {
+    final inputImage = InputImage.fromFilePath(image.path);
+    final textDetector = GoogleMlKit.vision.textRecognizer();
+    RecognizedText recognisedText = await textDetector.processImage(inputImage);
+    await textDetector.close();
+    scannedText = "";
+    for (TextBlock block in recognisedText.blocks) {
+      for (TextLine line in block.lines) {
+        scannedText = "$scannedText${line.text}\n";
+      }
+    }
+
+    textScanning = false;
+    setState(() {});
+    await flutterTts.setLanguage("en-US");
+    await flutterTts.speak(scannedText);
+  }
+
+  @override
+  void initState() {
+    super.initState();
   }
 }
